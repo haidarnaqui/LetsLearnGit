8628f59286854890a98e4c3428c565359425e942
Haidar Naqui <Haidarnaqui@gmail.com>
2022-03-25 01:59:26 -0700

    This is my 1st commit

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,sum;
+    printf("Enter the Two Number : ");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("Sum %d",sum);
+    getch();
+}
